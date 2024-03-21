1f2d2b22cb6f35538193aa8af2854b0044345e77
Bug <jownasbugado@gmail.com>
2024-03-21 15:52:58 -0300

    changed firebase auth from json file to .env variable to deploy on heroku

--- src/firebase_auth.py
+++ src/firebase_auth.py
@@ -1,7 +1,12 @@
+import os
 from firebase_admin import credentials, auth
 import firebase_admin
+import json
+from dotenv import load_dotenv
+load_dotenv()
 
-firebase_app = firebase_admin.initialize_app(credentials.Certificate("firebase_credentials.json"))
+firebase_credentials = json.loads(os.getenv('FIREBASE_CREDENTIALS'))
+firebase_app = firebase_admin.initialize_app(credentials.Certificate(firebase_credentials))
 
 def auth_user_token(user_token):
     try:
